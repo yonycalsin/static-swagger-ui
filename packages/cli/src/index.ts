0999245276df4362df2907274bf73815fbc45005
Yony Calsin <helloyonycalsin@gmail.com>
2023-02-23 23:05:59 -0500

    fix: remove the using of commander (#11)
    PR-URL: https://github.com/yonycalsin/static-swagger-ui/pull/11

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,17 +1,14 @@
 #!/usr/bin/env node
 
 import { generateStaticDist, setOpenapiFile } from '@static-swagger-ui/core'
-import { program } from 'commander'
 import figlet from 'figlet'
 
-figlet.textSync('Static Swagger Ui')
+async function main() {
+  figlet.textSync('Static Swagger Ui')
 
-program
-  .name('@static-swagger-ui')
-  .description('The cli for the static-swagger-ui')
-  .command('init')
-  .action(async () => {
-    await generateStaticDist()
+  await generateStaticDist()
 
-    await setOpenapiFile()
-  })
+  await setOpenapiFile()
+}
+
+main()
